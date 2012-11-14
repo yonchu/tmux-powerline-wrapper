85a26874166e359bfc6a1f0265535e7ca82d34c5
yonchu <yuyuchu3333@gmail.com>
2012-11-15 08:34:59 +0900

    Update for tmux-powerline(26035f104f). Add "fog" as a possible condition value.

--- segments/weather_yahoo.sh
+++ segments/weather_yahoo.sh
@@ -63,7 +63,7 @@ get_condition_symbol() {
             #echo "⚡"
             echo "☈"
         ;;
-    "dust" | "foggy" | "haze" | "smoky" | "blustery" | "mist")
+    "dust" | "foggy" | "fog" | "haze" | "smoky" | "blustery" | "mist")
         #echo "♨"
         #echo "﹌"
         echo "〰"
