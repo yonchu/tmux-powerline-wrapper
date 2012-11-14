b2965c43da77c2e9f13dda236c6c133ec86fa016
yonchu <yuyuchu3333@gmail.com>
2012-11-14 19:57:02 +0900

    Add new option USE_PATCHED_FONT

--- config.sh
+++ config.sh
@@ -25,6 +25,22 @@ if [ -z "$PLATFORM" ]; then
     esac
 fi
 
+# Set .tmux-powerlinerc.
+TMUX_POWERLINE_RC_FILE='.tmux-powerlinerc'
+
+# Source default .tmux-powerlinerc
+if [ -f "$TMUX_POWERLINE_WRAPPER_DIR/$TMUX_POWERLINE_RC_FILE" ]; then
+    source "$TMUX_POWERLINE_WRAPPER_DIR/$TMUX_POWERLINE_RC_FILE"
+fi
+
+# Source user .tmux-powerlinerc.
+if [ -f "$HOME/$TMUX_POWERLINE_RC_FILE" ]; then
+    source "$HOME/$TMUX_POWERLINE_RC_FILE"
+elif [ -f "$tmux_powerline_root_dir/$TMUX_POWERLINE_RC_FILE" ]; then
+    source "$tmux_powerline_root_dir/$TMUX_POWERLINE_RC_FILE"
+fi
+
+
 # Source tmux-powerline global configurations.
 source "${tmux_powerline_dir}/config.sh"
 
@@ -49,21 +65,6 @@ fi
 # Set tmux window width.
 window_width="$(${wrapper_segments_path}/window-width.sh)"
 
-
-# Set .tmux-powerlinerc.
-TMUX_POWERLINE_RC_FILE='.tmux-powerlinerc'
-
-# Source default .tmux-powerlinerc
-if [ -f "$TMUX_POWERLINE_WRAPPER_DIR/$TMUX_POWERLINE_RC_FILE" ]; then
-    source "$TMUX_POWERLINE_WRAPPER_DIR/$TMUX_POWERLINE_RC_FILE"
-fi
-
-# Source user .tmux-powerlinerc.
-if [ -f "$HOME/$TMUX_POWERLINE_RC_FILE" ]; then
-    source "$HOME/$TMUX_POWERLINE_RC_FILE"
-elif [ -f "$tmux_powerline_root_dir/$TMUX_POWERLINE_RC_FILE" ]; then
-    source "$tmux_powerline_root_dir/$TMUX_POWERLINE_RC_FILE"
-fi
 
 # Set .tmux-powerline-status-left.sh.
 TMUX_POWERLINE_STATUS_LEFT_FILE='.tmux-powerline-status-left.sh'
