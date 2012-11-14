51686e7d51966096ee90c2f042cfb10630461254
yonchu <yuyuchu3333@gmail.com>
2012-11-14 19:57:59 +0900

    Add new segments date-full.sh

---
+++ segments/date-full.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+# Prints the current date and time.(e.g. yyyy/mm/dd(a) HH:MM:SS)
+
+LANG=C date +"%Y/%m/%d(%a) %H:%M:%S"
+
+exit 0
--- segments/date.sh
+++ segments/date.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-# Prints the current date and time.
+# Prints the current date and time.(e.g. yy/mm/dd(a) HH:MM:SS)
 
 LANG=C date +"%y/%m/%d(%a) %H:%M:%S"
 
