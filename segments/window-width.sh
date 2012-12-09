eb3941a2345a3b962ae0077a3ee4de2f4fccb5c7
yonchu <yuyuchu3333@gmail.com>
2012-12-09 14:58:20 +0900

    Improve window_width.sh

--- segments/window-width.sh
+++ segments/window-width.sh
@@ -2,7 +2,7 @@
 # Prints the current window width.
 
 if [ -n "$TMUX" ]; then
-    tmux list-windows -F "#{window_active} #{window_width}" | sed '/^0/d' | cut -d' ' -f 2
+    tmux display -p "#{window_width}"
 fi
 
 exit 0
